44658b47c5f60f915cdffd15385c193e191d3f9f
SohyangO <shyang903@naver.com>
2024-01-22 12:06:18 +0900

    완료 버튼 클릭 시 취소, 취소 버튼 클릭 시 완료로 변경되게 구현

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [isDone, setIsDone] = useState(false);
 
   const inputTodo = (event) => {
+    // 제목에 내용 입력 시 실시간으로 출력
     setTodo(event.target.value);
   };
   const inputDetail = (event) => {
@@ -16,6 +17,15 @@ function App() {
   };
 
   const addTodo = () => {
+    // 추가하기 버튼 클릭 시 추가하기
+    if (todo.length === 0) {
+      alert("제목을 입력해주세요.");
+      return;
+    }
+    if (detail.length === 0) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
     setTodo("");
     setDetail("");
     const newTodo = {
@@ -24,12 +34,22 @@ function App() {
       detail,
       isDone: false,
     };
+    console.log(newTodo);
+    console.log(todoList);
     return setTodoList([...todoList, newTodo]);
   };
 
-  const onDelete = (id) => {
+  const deleteList = (id) => {
     const removedCard = todoList.filter((item) => item.id !== id);
     setTodoList(removedCard);
+  };
+
+  const changeState = (id) => {
+    //todo의 isDone 값을 업데이트하여 버튼 문구 변경
+    const updateState = todoList.map((newTodo) =>
+      newTodo.id === id ? { ...newTodo, isDone: !newTodo.isDone } : newTodo
+    );
+    setTodoList(updateState);
   };
 
   return (
@@ -47,20 +67,27 @@ function App() {
           추가하기
         </button>
       </div>
-      <div className="working">Working</div>
+      <div className="working">
+        <p>Working</p>
+      </div>
       {todoList.map((item) => {
         return [
           <div key={item.id} className="ing">
             <p>{item.todo}</p>
             <p>{item.detail}</p>
-            <button className="delete" onClick={() => onDelete(item.id)}>
+            <p>{item.id}</p>
+            <button className="delete" onClick={() => deleteList(item.id)}>
               삭제하기
             </button>
-            <button className="complete">완료</button>
+            <button className="complete" onClick={() => changeState(item.id)}>
+              {item.isDone ? "취소" : "완료"}
+            </button>
           </div>,
         ];
       })}
-      <div className="done">Done!</div>
+      <div className="done">
+        <p>Done!</p>
+      </div>
     </div>
   );
 }
